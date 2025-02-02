de6bc785409886adff1c59a68be91ea52a981038
Elias Herzog <elias.herzog.02@gmail.com>
2025-02-02 17:42:42 +0100

    above the fold beginning

--- src/app/above-the-fold/above-the-fold.component.ts
+++ src/app/above-the-fold/above-the-fold.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-above-the-fold',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
+
 export class AboveTheFoldComponent {
 
 }
