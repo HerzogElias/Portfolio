c106a747903f7b084ad0b95648c71e9ccc90c159
Elias Herzog <elias.herzog.02@gmail.com>
2025-02-03 22:48:15 +0100

    first why-me section

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
+import { WhyMeComponent } from "../why-me/why-me.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [AboveTheFoldComponent],
+  imports: [AboveTheFoldComponent, WhyMeComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
