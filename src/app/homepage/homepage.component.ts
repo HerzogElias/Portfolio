4e83f947fb5947a873da9fbb545e14db7415a38a
Elias Herzog <elias.herzog.02@gmail.com>
2025-02-15 17:21:02 +0100

    zwischenspeichern für fragestellung

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AboveTheFoldComponent } from '../above-the-fold/above-the-fold.component';
 import { WhyMeComponent } from "../why-me/why-me.component";
 import { MySkillsComponent } from "../my-skills/my-skills.component";
+import { MyProjectsComponent } from "../my-projects/my-projects.component";
 
 @Component({
   selector: 'app-homepage',
-  imports: [AboveTheFoldComponent, WhyMeComponent, MySkillsComponent],
+  imports: [AboveTheFoldComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
