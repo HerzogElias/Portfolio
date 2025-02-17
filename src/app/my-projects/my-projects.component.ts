ee9c88f9272f4321229c5cc2356d9d1091026df0
Elias Herzog <elias.herzog.02@gmail.com>
2025-02-17 18:02:28 +0100

    zwischenstand für frage

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
+ projects =[{
+    projectname:'sharky',
+    content:{
+      abouttheporject: 'Hier steht so zimlich alles was dem POrjekt zugute Kommt',
+      oranisation: 'Scrum macht alles möglich',
+      whatilerned: 'To oraganise a project with scrum'
+
+    },
+    technologies: 'Bild 1 und bild 2',
+    image: 'url to image',
+    github: 'githublink',
+    livetest: 'livetestlink' 
+ }]
+
+ projectnavbar:string[]=['Join', 'Sharky']
 
 }
