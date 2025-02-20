a7108c92ceb17982a094a7b6efc797ac143b6d86
Elias Herzog <elias.herzog.02@gmail.com>
2025-02-20 22:18:31 +0100

    zwischenstand

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
+import { SingleProjectComponent } from "./single-project/single-project.component";
 
 @Component({
   selector: 'app-my-projects',
-  imports: [],
+  imports: [SingleProjectComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
 export class MyProjectsComponent {
- projects =[{
-    projectname:'sharky',
-    content:{
+
+  selectedProject = 'Join';
+
+  projects = [{
+    projectname: 'sharky',
+    content: {
       abouttheporject: 'Hier steht so zimlich alles was dem POrjekt zugute Kommt',
       oranisation: 'Scrum macht alles möglich',
       whatilerned: 'To oraganise a project with scrum'
@@ -18,9 +22,26 @@ export class MyProjectsComponent {
     technologies: 'Bild 1 und bild 2',
     image: 'url to image',
     github: 'githublink',
-    livetest: 'livetestlink' 
- }]
+    livetest: 'livetestlink'
+  },
+  {
+    projectname: 'Join',
+    content: {
+      abouttheproject: 'Hier steht so ziemlich alles was dem POjekt zugute kommt',
+      organisation: 'Scrum macht alles möglich',
+      whatilearned: 'To organise a project with scrum'
+    },
+    technologies: 'Bild 1 und bild 2',
+    image: 'url to image',
+    github: 'githublink',
+    livetest: 'livetestlink'
+  }
+  ]
 
- projectnavbar:string[]=['Join', 'Sharky']
+  projectnavbar: string[] = ['Join', 'Sharky']
 
+
+  setSelectedProject(projectname: string) {
+    this.selectedProject = projectname;
+  }
 }
