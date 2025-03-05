af481bc7fe3a750cd5f264adb5f804122d4dee3a
Elias Herzog <elias.herzog.02@gmail.com>
2025-03-05 22:39:54 +0100

    fill in objekcts for project.component with real data

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -16,26 +16,26 @@ export class MyProjectsComponent {
   projects: Projekt[] = [{
     projectname: 'Join',
     content: {
-      aboutTheProject: 'Hier steht so ziemlich alles was dem POjekt zugute kommt',
-      organisation: 'Scrum macht alles möglich',
-      whatILearned: 'To organise a project with scrum'
+      aboutTheProject: 'Join hilft dir als richtiges Aufgabentool deine Aufgaben mit einem Scrum-Board zu pflegen, sowie deine Kontakte zu verwakten',
+      organisation: 'Durch eine Gruppenarbeit mit 3 Personen wurden die Aufgaben mit der Hilfe eines Scrum-Board gemanaged und regelmäßige Termine gefplegt',
+      whatILearned: 'Druch das Projekt erste Berührungspunkte mit Firebase erhalten und zu lernen wie man ein Projekt mit der Scrum Mehtode managed'
     },
-    technologies: [],
-    images: [],
-    github: 'githublink',
-    livetest: 'livetestlink'
+    technologies: ['Firebase', 'CSS', 'Javascript'],
+    images: ['/img/myskills/firebase.png', '/img/myskills/css.png', '/img/myskills/javascript.png'],
+    github: 'github.com/herzogelias/join',
+    livetest: 'join.elias-herzog.de'
   },
   {
-    projectname: 'sharky',
+    projectname: 'Sharky',
     content: {
-      aboutTheProject: 'Hier',
-      organisation: 'Scrum macht alles möglich',
-      whatILearned: 'To organise a project with Scrum'
+      aboutTheProject: 'Mit der Hilfe von Obektorientierten Javascript wurde ein 2D Geme programmiert. Hilf den Shakry, Gift zu sammeln, um den Endboss zu töten. ',
+      organisation: 'Was soll ich hier reinschreiben ich weiß es noch nicht',
+      whatILearned: 'Erste Tiefgehende Erfahrungen gesammelt in die Welt der Objetorientierten Javascript Programmeirung und mit Canvas'
     },
-    technologies: ['Bild 1', 'Bild 2'],
-    images: ['url-to-image'],
-    github: 'githublink',
-    livetest: 'livetestlink'
+    technologies: ['HTML', 'CSS', 'Javascript'],
+    images: ['/img/myskills/html.png','/img/myskills/css.png', '/img/myskills/javascript.png' ],
+    github: 'github.com/herzogelias/sharky',
+    livetest: 'dukessharkygame.elias-herzog.de'
   }
   ]
 
