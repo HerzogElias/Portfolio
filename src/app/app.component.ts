a6933b7b307f071774d47fb351bcc64c63de763d
Elias Herzog <elias.herzog.02@gmail.com>
2025-04-08 18:30:14 +0200

    design contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Projekt } from "./interfaces/projects.interface";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { BrowserModule } from '@angular/platform-browser';
 
 
 @Component({
