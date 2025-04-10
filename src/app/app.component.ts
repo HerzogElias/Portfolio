33b130a0a0242666642932e873454f9804b07e2f
Elias Herzog <elias.herzog.02@gmail.com>
2025-04-10 16:43:55 +0200

    begining translation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,24 @@
-import { Component, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Projekt } from "./interfaces/projects.interface";
 import { FooterComponent } from "./shared/footer/footer.component";
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslateModule} from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent],
+  imports: [RouterOutlet, FooterComponent, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'portfolio';
+
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('de');
+    this.translate.use('en');
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,27 @@
-import { ApplicationConfig, provideZoneChangeDetection, TRANSLATIONS } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// Factory-Funktion für den TranslateLoader
+const httpLoaderFactory = (http: HttpClient): TranslateHttpLoader =>
+  new TranslateHttpLoader(http, './i18n/', '.json');
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: httpLoaderFactory,
+          deps: [HttpClient],
+        },
+      })
+    ),
+  ],
 };
