c515dc51429d0e8a49ee3cb5e2987ec192cfa156
Elias Herzog <elias.herzog.02@gmail.com>
2025-04-24 13:44:15 +0200

    tootlge translation

--- src/app/above-the-fold/navbar/navbar.component.ts
+++ src/app/above-the-fold/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +8,14 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(private translate:TranslateService) {
+    this.translate.setDefaultLang('de');
+    this.translate.use('de');
+  }
 
+  toogleTranslation(languae:string){
+    this.translate.use(languae);
+    const CURRENT_LANGUAGE = this.translate.currentLang;
+    console.log(CURRENT_LANGUAGE);
+  }
 }
