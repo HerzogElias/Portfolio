1d5b5ccfb0f0612815997e983e196d20ffbb005a
Elias Herzog <elias.herzog.02@gmail.com>
2025-05-26 17:30:54 +0200

    trying some other routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from "./shared/footer/footer.component";
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule} from '@ngx-translate/core';
 import * as AOS from 'aos';
+import { NavbarComponent } from "./above-the-fold/navbar/navbar.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FooterComponent, TranslateModule],
+  imports: [RouterOutlet, FooterComponent, TranslateModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
